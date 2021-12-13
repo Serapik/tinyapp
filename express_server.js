ad791fdb1f6a6ee888710b5eb972441f6a81ee9a
Fadime Serap Keskinler <fsekurt@gmail.com>
2021-12-12 20:13:34 -0500

    Tiny App

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -15,29 +16,42 @@ app.get("/", (req, res) => {
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  //res.render("urls_new");
+  const templateVars = {
+    username: req.cookies['username']
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL,
-    urlUser: urlDatabase[req.params.shortURL].userID,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username']
   };
-  res.render('urls_show', templateVars);
   
-  app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL];
-    res.redirect(longURL);
-  });
-
+  res.render('urls_show', templateVars);
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('urls');
+});
+  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
